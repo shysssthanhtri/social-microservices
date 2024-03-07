2a252298252e7a8f995d8ea193cd55598124cc20
tri-nguyen-topicus <Tri.Nguyen.Thanh@topicus.nl>
2024-03-07 10:37:04 +0700

    feat: integrate validation pipe

--- apps/users/src/dto/create-user.input.ts
+++ apps/users/src/dto/create-user.input.ts
@@ -1,7 +1,9 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
   @Field()
+  @IsEmail()
   email: string;
 }
--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,9 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
-import { UsersModule } from './users.module';
+import { UsersModule } from 'apps/users/src/users.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(UsersModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
